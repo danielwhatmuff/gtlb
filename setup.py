6e82e57e08f0749730db3304de4104650ccdc0c1
Daniel Whatmuff <danielwhatmuff@gmail.com>
2018-11-06 10:49:24 +0000

    flake8ing setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@
 Setup.py for gtlb
 """
 
-from setuptools import setup, find_packages
-from codecs import open
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 setup(
     name='gtlb',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     long_description='A CLI to open Gitlab project in a browser',
     url='https://github.com/danielwhatmuff/gtlb',
@@ -22,6 +21,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='git gtlb gitlab cli open repo commandline browser',
     py_modules=["gtlb"],
